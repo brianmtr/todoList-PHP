be9fad6e3b2513487aa415d750e4251c0316f8f5
Brian <brian.mtr@outlook.fr>
2022-04-28 09:23:36 +0200

    add welcome file

---
+++ includes/footer.php
@@ -0,0 +1,3 @@
+<footer>
+    <p>2021 / 2022 © Tous droit réservés</p>
+</footer>
---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php require_once 'includes/head.php' ?>
+    <title>Todo - Accueil</title>
+</head>    
+
+<body>
+    <div class="container">
+        <?php require_once 'includes/header.php' ?>
+        
+        <div class="content">
+            <div class="todo-container">
+                <h1>Ma todo</h1>
+                <div class="todo-form"></div>
+                <div class="todo-list"></div>
+            </div>
+        </div>
+        <?php require_once 'includes/footer.php' ?>
+    </div>
+</body>
+</html>
+
+
+
